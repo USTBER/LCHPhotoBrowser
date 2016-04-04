5ce91d6622c1ca1ddf83267c9ee86ba093426d0f
Lee <532557007@qq.com>
2016-04-04 22:31:34 +0800

    Initial Commit

---
+++ LCHPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCHPhotoBrowser
+//
+//  Created by LiChunxing on 16/4/4.
+//  Copyright © 2016年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LCHPhotoBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LCHPhotoBrowser
+//
+//  Created by LiChunxing on 16/4/4.
+//  Copyright © 2016年 Demo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
